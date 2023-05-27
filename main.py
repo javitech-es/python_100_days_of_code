059d41540f9a1668b6515488b4f93b22a429442d
Javier Fernández González <javier@javitech.es>
2023-05-28 01:55:34 +0200

    First upload. Testing print function

---
+++ main.py
@@ -0,0 +1,8 @@
+#Day 1, Printing in the console in Python
+print("Hola mundo")
+
+#Interactive exercise 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
