d52748b135ffae7c199f8b97a3a21243f240bdd4
Javier Fernández González <javier@javitech.es>
2023-05-30 23:53:15 +0200

    More simple exercises

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+#Line jump
+print("Hola Mundo\nHola Mundo 2")
+
+#Concatenate strings
+print("Hola "+"Mundo")
+
